426b32f1ec2df124b3bed5edb8212c39ab1b52c3
Johannes Schickling <schickling.j@gmail.com>
2014-01-12 13:58:25 +0100

    Dummy test

--- tests/InvalidationRulesTest.php
+++ tests/InvalidationRulesTest.php
@@ -1,23 +1,9 @@
-<?php namespace Services\Cash;
+<?php
 
-use Cache;
-use Memcached;
-use Request;
-
-class CashFilter {
-
-    public function filter($route, $request, $response)
+class InvalidationRulesTest  extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
     {
-        if (Request::getMethod() == 'GET')
-        {
-            $path = '/' . Request::path();
-            $content = $response->getContent();
-            $cache = Cache::driver('memcached');
-            $memcached = $cache->getMemcached();
-            $memcached->setOption(Memcached::OPT_COMPRESSION, false);
-            $cache->put($path, $content, 0);
-        }
-    } 
 
+    }
 }
-
