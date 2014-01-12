c79d9802dd7a7835ab52902bd9fdfcb06fbee546
Johannes Schickling <schickling.j@gmail.com>
2014-01-12 22:28:27 +0100

    Caching coverage

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -7,8 +7,50 @@ use Mockery as m;
 class StoreTest extends TestCase
 {
 
-    public function testSomething()
+    public function setUp() 
     {
+        parent::setUp();
+
+        Route::get('hello', array('after' => 'cash', function()
+        {
+            return 'Hello World';
+        }));
+
+        $this->app['router']->enableFilters();
+        
+        $memcachedMock = m::mock(array('setOption' => null));
+        \MemcachedInstance::shouldReceive('getMemcached')
+                            ->once()
+                            ->andReturn($memcachedMock);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        m::close();
+    }
+    
+    protected function getPackageProviders()
+    {
+        return array('Schickling\Cash\CashServiceProvider');
+    }
+
+    public function testStoring()
+    {
+        \MemcachedInstance::shouldReceive('put')
+                            ->with('/hello', 'Hello World', 0)
+                            ->once();
+        
+        $this->call('GET', 'hello');
+    }
+
+    public function testStoringWithPrependingSlash()
+    {
+        \MemcachedInstance::shouldReceive('put')
+                            ->with('/hello', 'Hello World', 0)
+                            ->once();
+        
+        $this->call('GET', '/hello');
     }
 
 }
