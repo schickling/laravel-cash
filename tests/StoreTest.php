bc9dcdf64efd230e46825af75a9a0f7c9fc9365b
Johannes Schickling <schickling.j@gmail.com>
2014-01-12 22:16:26 +0100

    Split tests

---
+++ tests/StoreTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Schickling\Cash\Facades\Cash;
+use Orchestra\Testbench\TestCase;
+use Mockery as m;
+
+class StoreTest extends TestCase
+{
+
+    public function testSomething()
+    {
+    }
+
+}
